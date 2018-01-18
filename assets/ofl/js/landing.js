39f06e39d973b0ef9d09e8f9362f543ea1ba57ef
Oyesola Ogundele <oyekunmi@gmail.com>
2018-01-18 14:15:00 +0100

    Responsive fixes on Search.html

---
+++ assets/ofl/js/landing.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+    $('.bxslider, .bxslider-featured-one').bxSlider({
+      pager: false
+    });
+    $('.bxslider2, .bxslider3').bxSlider({
+      controls:false,
+      auto:true,
+      autoStart:true,
+      autoHover:true
+    });
+
+    $('a[href="#tab-two"]').one('shown.bs.tab', function (e) {
+      $('.bxslider4').bxSlider({
+        controls:false,
+        auto:true,
+        autoStart:true,
+        autoHover:true
+      });
+    });
+
+    $('a[href="#tab-three"]').one('shown.bs.tab', function (e) {
+      $('.bxslider5').bxSlider({
+        controls:false,
+        auto:true,
+        autoStart:true,
+        autoHover:true
+        });
+    });
+
+
+    $('a[href="#tab-featured-two"]').one('shown.bs.tab', function (e) {
+      $('.bxslider-featured-two').bxSlider({
+        pager: false            
+      });
+    });
+
+
+    $('a[href="#tab-featured-three"]').one('shown.bs.tab', function (e) {
+      $('.bxslider-featured-three').bxSlider({
+        pager: false            
+      });
+    });
+
+});
